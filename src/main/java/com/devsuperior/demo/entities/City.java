202f1e6303f52b230dd8f3bd35eb6db2b4fc4313
Samuel Quaresma <squoliver@gmail.com>
2023-09-28 16:34:29 -0300

    Ajuste de imports e identações

--- src/main/java/com/devsuperior/demo/entities/City.java
+++ src/main/java/com/devsuperior/demo/entities/City.java
@@ -1,8 +1,5 @@
 package com.devsuperior.demo.entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,43 +7,46 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tb_city")
 public class City {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	
-	@OneToMany(mappedBy = "city")
-	private List<Event> events = new ArrayList<>();
-	
-	public City() {
-	}
 
-	public City(Long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 
-	public Long getId() {
-		return id;
-	}
+    @OneToMany(mappedBy = "city")
+    private List<Event> events = new ArrayList<>();
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public City() {
+    }
 
-	public String getName() {
-		return name;
-	}
+    public City(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public List<Event> getEvents() {
-		return events;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
 }
